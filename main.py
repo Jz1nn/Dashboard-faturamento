1d52942b317504cdb1710fb482b1c2777a53c355
Jz1nn <jz1nnwln@gmail.com>
2023-03-24 12:21:15 -0300

    Feito importacao, tratamendo e compilacao da base de dados.Realizado calculos de faturamento de produtos e lojas

--- main.py
+++ main.py
@@ -6,14 +6,34 @@ import plotly.graph_objects as go
 
 # Percorrer todos os arquivos da pasta base de dados (Vendas)
 
-# Importar base de dados de vendas
+lista_arquivo = os.listdir('C:\\Users\\benbo\\OneDrive\\Área de Trabalho\\PROJETOS\\Dashboard-vendas-devolucoes\\Vendas')
+
+# Importar base de dados de vendas para um DataFrame
+
+tabela_total = pd.DataFrame()
 
 # Tratamento e compilação das bases de dados
 
+for arquivo in lista_arquivo:
+    if "Vendas" in arquivo:
+        tabela = pd.read_csv(f"C:\\Users\\benbo\\OneDrive\\Área de Trabalho\\PROJETOS\\PROJETOS\\Dashboard-vendas-devolucoes\\Vendas\\{arquivo}")
+        tabela_total = pd.concat([tabela_total, tabela], ignore_index=True)
+
 # Calcular o produto mais vendido (em quantidade)
+
+tabela_produtos = tabela_total.groupby('Produto').sum()
+tabela_produtos = tabela_produtos[["Quantidade Vendida"]].sort_values(by="Quantidade Vendida", ascending=False)
 
 # Calcular o produto que mais faturou (em faturamento)
 
+tabela_total['Faturamento'] = tabela_total['Quantidade Vendida'] * tabela_total['Preco Unitario']
+
+tabela_faturamento = tabela_total.groupby('Produto').sum()
+tabela_faturamento = tabela_faturamento[["Faturamento"]].sort_values(by="Faturamento", ascending=False)
+
 # Calcular a loja/cidade que mais vendeu (em faturamento)
+
+tabela_lojas = tabela_total.groupby('Loja').sum()
+tabela_lojas = tabela_lojas[["Faturamento"]].sort_values(by="Faturamento", ascending=False)
 
 # Mostrar gráfico
