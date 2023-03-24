d43b1b0fef90aee78c2d147b4234c7189dee7ef7
Jz1nn <jz1nnwln@gmail.com>
2023-03-24 12:02:10 -0300

    Criado arquivo principal do codigo contendo as bibliotecas e escopo inicial do projeto

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+# Percorrer todos os arquivos da pasta base de dados (Vendas)
+
+# Importar base de dados de vendas
+
+# Tratamento e compilação das bases de dados
+
+# Calcular o produto mais vendido (em quantidade)
+
+# Calcular o produto que mais faturou (em faturamento)
+
+# Calcular a loja/cidade que mais vendeu (em faturamento)
+
+# Mostrar gráfico
