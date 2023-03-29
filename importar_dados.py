3c2f193277dbb10866fd32e761a5f015f857618c
Jz1nn <jz1nnwln@gmail.com>
2023-03-29 14:00:02 -0300

    Alteracoes informadas na anotacao 28/03 do README

--- importar_dados.py
+++ importar_dados.py
@@ -1,13 +1,19 @@
-# Retornar tabela total com os dados de todas as vendas
+# Retornar tabela total com todos os dados
 
 import os
 import pandas as pd
 
-def importar_dados():
-    lista_arquivo = os.listdir(os.path.join(os.getcwd(), 'Vendas'))
-    tabela_total = pd.DataFrame()
-    for arquivo in lista_arquivo:
-        if "Vendas" in lista_arquivo:
-            tabela = pd.read_csv(os.path.join(os.getcwd(), 'Vendas', arquivo))
-            tabela_total = pd.concat([tabela_total, tabela], ignore_index=True)
-    return tabela_total
+class importarDados:
+    def __init__(self):
+        self.caminho = os.path.join(os.getcwd(), 'Vendas')
+
+    def importar_dados(self):
+        lista_arquivos = os.listdir(self.caminho)
+        tabela_total = pd.DataFrame()
+        
+        for arquivo in lista_arquivos:
+            if "Vendas" in arquivo:
+                tabela = pd.read_csv(os.path.join(self.caminho, arquivo))
+                tabela_total = pd.concat([tabela_total, tabela], ignore_index=True)
+
+        return tabela_total
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Importar os modulos e executar as funções para gerar as tabelas
 
-import importar_dados
-import calcular_produtos
-import calcular_lojas
-import criar_grafico
+from importar_dados import importar_dados as impd
+from calcular_produtos import CalculoProdutos
+from calcular_lojas import CalculoLojas
 
-tabela_total = importar_dados.importar_dados()
-tabela_produtos = calcular_produtos.calcular_produtos_vendidos(tabela_total)
-tabela_faturamento = calcular_produtos.calcular_faturamento_produtos(tabela_total)
-tabela_lojas = calcular_lojas.calcular_lojas_mais_vendem(tabela_total)
-fig = criar_grafico.criar_dashboard(tabela_produtos, tabela_faturamento, tabela_lojas)
-fig.show()
+tabela_total = impd().importar_dados()
+tabela_produtos = CalculoProdutos(tabela_total).calcular_produtos_vendidos()
+tabela_faturamento = CalculoProdutos(tabela_total).calcular_faturamento_produtos()
+tabela_lojas = CalculoLojas(tabela_total).calcular_lojas_mais_vendem()
+# Criar o grafico
 
-# Salvar o dashboard em um arquivo html
-fig.write_html('dashboard.html')
+from criar_grafico import criar_grafico
+
+criar_grafico().criar_grafico(tabela_total)
+
