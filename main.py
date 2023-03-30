fb645791eec0e94b6c5f9a8f042155c1929fe9d5
Jz1nn <jz1nnwln@gmail.com>
2023-03-29 23:13:36 -0300

    Modulo main ainda nao esta respondendo como deveria

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# Importar os modulos e executar as funções para gerar as tabelas
+from importar_dados import importarDados
+from produtos_mais_vendidos import produtosMaisVendidos
+from produtos_mais_faturaram import produtosMaisFaturaram
+from lojas_mais_venderam import lojasMaisVenderam
+from criar_dashboard import CriarDashboard
 
-from importar_dados import importar_dados as impd
-from calcular_produtos import CalculoProdutos
-from calcular_lojas import CalculoLojas
-
-tabela_total = impd().importar_dados()
-tabela_produtos = CalculoProdutos(tabela_total).calcular_produtos_vendidos()
-tabela_faturamento = CalculoProdutos(tabela_total).calcular_faturamento_produtos()
-tabela_lojas = CalculoLojas(tabela_total).calcular_lojas_mais_vendem()
-# Criar o grafico
-
-from criar_grafico import criar_grafico
-
-criar_grafico().criar_grafico(tabela_total)
-
+importador = importarDados()
+tabela_total = importador.importar_dados()
